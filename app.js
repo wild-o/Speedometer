6aa16b40f020a2ac894fe08a8391e4b2cfd68aa3
crazyDog <alarias1@outlook.com>
2023-05-16 19:52:22 -0500

    Styles Changes, Gear Functionality implemented

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const newNum = document.getElementsByClassName("gauge-reading")[0];
 const gas = document.getElementById("pedal");
 const brake = document.getElementById("brake");
+const shiftChange = document.getElementById("gear");
+const gearHUD = document.getElementById("gear-notification");
 
 gas.addEventListener("mousedown", Accelerate);
 gas.addEventListener("mouseup", Decelerate);
 brake.addEventListener("mousedown", Brake);
+shiftChange.addEventListener("click", gearShift);
 
 let LINEAR_ACC = 1 / 2;
 let UPDATE = 0;
-let interval = 75;
+let interval = 90;
 let throttle = true;
 
-let interv = setInterval(() => {
-  if (UPDATE > 0 && UPDATE < 200 && throttle == true) {
+let lowerBound = 0;
+let upperBound = 25;
+let shiftCount = 1;
+
+let interv = setInterval((x = lowerBound, y = upperBound) => {
+  if (UPDATE > x && UPDATE < y && throttle == true) {
     UPDATE += LINEAR_ACC;
     newNum.innerHTML = UPDATE;
   }
@@ -22,14 +29,16 @@ function Accelerate() {
   throttle = true;
   UPDATE++;
   LINEAR_ACC = 1;
+
+  console.log(UPDATE);
 }
 
-function Decelerate() {
+function Decelerate(x = lowerBound, y = upperBound) {
   LINEAR_ACC = -1;
 
-  if (UPDATE === 0) UPDATE = 1;
+  if (UPDATE === x) UPDATE = x + 1;
   // Add to decelerate
-  if (UPDATE === 200) UPDATE = 199;
+  if (UPDATE === y) UPDATE = y - 1;
 
   console.log(UPDATE);
 }
@@ -37,9 +46,9 @@ function Decelerate() {
 function Brake() {
   throttle = false;
 
-  var myFunction = function () {
-    if (UPDATE > 0 && UPDATE < 200 && throttle == false) {
-      interval = 40;
+  var myFunction = function (x = lowerBound, y = upperBound) {
+    if (UPDATE > x && UPDATE < y && throttle == false) {
+      interval = 25;
 
       UPDATE += LINEAR_ACC;
       newNum.innerHTML = UPDATE;
@@ -47,6 +56,23 @@ function Brake() {
     }
   };
   setTimeout(myFunction, interval);
+}
+
+function gearShift() {
+  if (!(upperBound >= 150)) {
+    upperBound += 25;
+    shiftCount++;
+    console.log(`You are now in ${shiftCount} gear!`);
+    gearHUD.innerHTML = `You are now in gear position:  ${shiftCount}`;
+    
+  } else {
+    lowerBound = 0;
+    upperBound = 25;
+    shiftCount = 1;
+    UPDATE = 0;
+    newNum.innerHTML = UPDATE;
+    gearHUD.innerHTML = `You are now in gear position:  ${shiftCount}`;
+  }
 }
 
 //Discovery: appending innerHTML a millisecond slower makes
@@ -59,7 +85,6 @@ function Brake() {
 
 // Accelerate and Declerate need to be able to share the speed value
 // Now I can perhaps use the global variable UPDATE
-
 
 //On the Brake function, it was suggested to use a recursive function
 //Not fully understanding why this works just yet:
